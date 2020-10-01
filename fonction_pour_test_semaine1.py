9d3f847913e285afd0c66d774ae80885e0311969
Manirakiza <30662277+manirakiza@users.noreply.github.com>
2020-10-02 01:04:36 +0200

    Update fonction_pour_test_semaine1.py

--- fonction_pour_test_semaine1.py
+++ fonction_pour_test_semaine1.py
@@ -1,7 +1,7 @@
 import unittest
 import semaine1_test as tf
 mon_dict= {'A': ['B', 'C'], 'B': ['C', 'D'], 'D': ['E']}
-fichier_test="D:/M2 2020-2021/Reseaux Biologiques Projet/toy_example.txt"
+fichier_test='D:/M2 2020-2021/Reseaux Biologiques Projet/toy_example.txt'
 class TestOfFunction(unittest.TestCase):
     def test_read_interaction_file_dict(self):
         #"on vérifie que la focntion fonctionne "
