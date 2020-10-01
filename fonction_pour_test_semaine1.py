5327b319587d125f622474b048649186951872c3
Manirakiza <30662277+manirakiza@users.noreply.github.com>
2020-10-02 01:03:22 +0200

    Add files via upload

---
+++ fonction_pour_test_semaine1.py
@@ -0,0 +1,9 @@
+import unittest
+import semaine1_test as tf
+mon_dict= {'A': ['B', 'C'], 'B': ['C', 'D'], 'D': ['E']}
+fichier_test="D:/M2 2020-2021/Reseaux Biologiques Projet/toy_example.txt"
+class TestOfFunction(unittest.TestCase):
+    def test_read_interaction_file_dict(self):
+        #"on vérifie que la focntion fonctionne "
+      self.assertEqual(tf.read_interaction_file_dict(fichier_test),mon_dict)
+    
